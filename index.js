1629dd93b92639d68066874d08dca5a1802a08e2
Andrew Cravenho <andrew@cravenho.me>
2019-05-11 15:16:35 -0400

    Initial Commit :tada:

---
+++ index.js
@@ -0,0 +1,32 @@
+const Twit = require('twit')
+const Twitter = new Twit(require('./config.js'))
+let stream = Twitter.stream('user')
+let sourceCode = 'https://github.com/acravenho/xdai-twitterbot'
+
+stream.on('follow', followed)
+stream.on('message', message)
+stream.on('direct_message', direct_message)
+
+// callback function to DM wallet info to new user when followed
+function followed (data) {
+  let name = data.source.name
+  let screenName = data.source.screen_name
+  let response = 'Thanks for following the xDAI Twitter Bot, ' + name + ' @' + screenName + '. Your wallet information has been sent. Source code located here: ' + sourceCode
+  
+  // Post Twitter Message
+  Twitter.post('statuses/update', { status: response }, tweeted)
+
+  // DM user 
+
+  console.log('Followed by: ' + name + ' @' + screenName)
+}
+
+// callback function to DM wallet info to new user when messaged
+function message (data) {
+
+}
+
+// callbback function to DM wallet info to new user when direct messaged
+function direct_message (data) {
+
+}
